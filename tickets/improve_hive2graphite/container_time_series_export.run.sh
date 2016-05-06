437abce7d71be1149ce997945b3225d4388176ed
Thomas Nystrand <tnystrand@altiscale.com>
2016-05-06 11:31:06 +0000

    ticket me

--- tickets/improve_hive2graphite/container_time_series_export.run.sh
+++ tickets/improve_hive2graphite/container_time_series_export.run.sh
@@ -1,1 +1,6 @@
-hiveq -f container_time_series_export.q -hiveconf DB_NAME=cluster_metrics_prod_2 -hiveconf TMP_DIR=/user/tnystrand/
+python /home/tnystrand/usefulscripts/besthivequeuelaunch.py \
+    -f container_time_series_export.q \
+    -hiveconf DB_NAME=cluster_metrics_prod_2 \
+    -hiveconf TMP_DIR=/tmp/tnystrand/ \
+    -hiveconf start_date='2016-04-01' \
+    -hiveconf end_date='2016-04-01'
