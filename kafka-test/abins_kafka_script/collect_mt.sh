cb8225166daf03ee889b3fbb7c48d81b3851906e
Thomas Nystrand <tnystrand@altiscale.com>
2016-05-30 22:59:04 +0200

    blabla

--- kafka-test/abins_kafka_script/collect_mt.sh
+++ kafka-test/abins_kafka_script/collect_mt.sh
@@ -1,6 +1,5 @@
 ./kafka_parse_metrics show \
-    --topic resourcemanager \
+    --topic mt-burst-metrics \
     --timestamp 1463702400000 \
-    --cluster-queues playfirst:default \
     --path /Users/admin/datapipeline_dump/kafka-test/abins_kafka_script/abins_data/data_1463702400000 \
     --kafka kafka01-sc1.service.altiscale.com:9092
