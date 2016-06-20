3b229e27a8579809fe8feea899ea25d852bd122e
Thomas Nystrand <tnystrand@altiscale.com>
2016-06-20 18:55:58 +0200

    backing

---
+++ consumer/burst_support/run_remote_query.sh
@@ -0,0 +1,14 @@
+set -e
+
+ssh dfs << EOF
+hive \
+--hiveconf hive.execution.engine=tez \
+--hiveconf hive.tez.java.opts=-Xmx7096m \
+--hiveconf hive.tez.container.size=8000 \
+--hiveconf tez.cpu.vcores=4 \
+--hiveconf tez.session.client.timeout.secs=10000000 \
+--hiveconf tez.session.am.dag.submit.timeout.secs=10000000 \
+--hiveconf tez.queue.name=research \
+-e "select * from cluster_metrics_prod_2.burst_time_series where partition_date>'2016-06-06'" > /tmp/thomas/burst_time_series.tsv
+exit
+scp tnystrand@dfs://tmp/thomas/burst_time_series.tsv .
